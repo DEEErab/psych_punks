1fab01bb42589c49184293e41104414c7169de01
Dalton Phillips <daltonphillips1020@gmail.com>
2021-09-22 23:37:21 -0400

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,40 +1,22 @@
-import csv
 import os
-import json
-import pandas as pd
 
 dirname = os.path.dirname(__file__)
 
-csv_file = pd.read_csv(dirname + '/data/punks.csv')
-x=0
-z=1
-BASE_URI = 'https://gateway.pinata.cloud/ipfs/Qmb5F9Q5UoUEeAB4hfeE55FU7y2VzT7t1gkm3bXmGGHnj6'
-
+x=1
 
 while x <= 100:
-    csv_file.loc[x,'image:'] = BASE_URI + '/PsychPunk_' + str(z) + '.png'
-    
-    x += 1
-    z += 1
-csv_file.to_csv(dirname + '/data/metadata/punks.csv', index=False)
 
-
-csv_file = open(dirname + '/data/metadata/punks.csv','r')
-csv_reader = csv.DictReader(csv_file,fieldnames = ('image:','Name:','Background:', 'Type:', 'Mouth:', 'Accessory:', 'Eyes:', 'Hair:', 'Beard:', 'Psych DNA:'))
-
-
-
-
-lcount = 0
-for row in csv_reader:
-    out = json.dumps(row, indent=4)
-
-    jsonoutput = open(dirname + '/data/metadata/PsychPunk_'+str(lcount)+'.json','w')
-    jsonoutput.write(out)
-    lcount+=1
-os.remove(dirname + '/data/metadata/PsychPunk_0.json')
-jsonoutput.close()
-csv_file.close()
-
-print('metadata created')
-print('----------------')
+	fin = open(dirname + '/data/metadata/origonal_data'+str(x)+'.json', 'rt')
+	#output file to write the result to
+	fout = open(dirname + '/data/metadata/edited_metadata/'+str(x)+'.json', "wt")
+	#for each line in the input file
+	for line in fin:
+		#read replace the string and write to output file
+		fout.write(line.replace('""', '"https://gateway.pinata.cloud/ipfs/Qma1rfddbns6hfqwf2GTxQwfYtREM6LXTHgDKatAEabsHC/PsychPunk_'+str(x)+'.png"'))
+	#close input and output files
+	fin.close()
+	fout.close()
+	x+=1
+	
+print('metadata edited')
+print('---------------')
